63591a3e2e94a5bef06cdf50ab13b20800e3c295
Serkan <94819174+Serkangur@users.noreply.github.com>
2021-11-23 07:20:44 +0300

    C# The multiplication table
    C# The multiplication table

---
+++ çarpımtablosu/çarpımtablosu/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace çarpımtablosu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 10; a++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine(a + "x" + j + "=" + (a * j));
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
